9457580864b90766033f6feac6ea8ec1c5011ed7
Lasse Nielsen <lasse@xerrion.dk>
2024-10-21 08:24:47 +0200

    Update main.tf

--- terraform-deployment/main.tf
+++ terraform-deployment/main.tf
@@ -2,21 +2,14 @@ provider "aws" {
   region = "eu-north-1" # Stockholm region
 }
 
-# Define a security group to allow SSH and HTTP access
-resource "aws_security_group" "allow_ssh_http" {
-  name        = "allow_ssh_http"
-  description = "Allow SSH and HTTP traffic"
+# Define a security group to allow SSH access
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH traffic"
 
   ingress {
     from_port   = 22
     to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -47,14 +40,14 @@ resource "aws_instance" "scraper_instance" {
               sudo amazon-linux-extras install docker
               sudo service docker start
               sudo usermod -a -G docker ec2-user
-              sudo docker run -d -p 80:8000 ${var.docker_image_name}
+              sudo docker run -d ${var.docker_image_name}
               EOF
 }
 
 # Define the key pair to SSH into the instance
 resource "aws_key_pair" "scraper_key_pair" {
   key_name   = "scraper-key"
-  public_key = file("~/.ssh/id_rsa.pub") # Replace with your actual public key path
+  public_key = file("~/.ssh/id_rsa.pub")
 }
 
 output "instance_ip" {
